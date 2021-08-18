62e1997f96d3d94105841e5b13ecc5a3b1d99b6a
Jarvis-xuhui-du <xudu8251@uni.sydney.edu.au>
2021-08-18 13:34:28 +1000

    Proof of session 2

---
+++ sayMyName.py
@@ -0,0 +1,11 @@
+## This program will print the name n times(n=first two digit of SID)
+
+
+name = "Jarvis"
+
+print("What's your first two digits of SID: ")
+SID = input()
+n = int(SID)
+
+for i in range(n):
+    print(name)
